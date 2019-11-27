6614805c49491539731e9e3433ac73bb3c01d7d6
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-11-27 14:00:57 -0500

    add scroll on reveal animations

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -5,3 +5,10 @@ import MobileMenu from './modules/MobileMenu';
 const widescreenMenu = new WidescreenMenu();
 const backToTop = new BackToTop();
 const mobileMenu = new MobileMenu();
+
+ScrollReveal().reveal('.hero', {delay: 100, duration: 1500});
+ScrollReveal().reveal('.hero__content-box', {delay: 1500, duration: 2000});
+ScrollReveal().reveal('.hero__logo', {delay: 2500, duration: 1000});
+ScrollReveal().reveal('.scroll-reveal--up', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }});
+ScrollReveal().reveal('.participate__list-item', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500, origin: 'right'});
+ScrollReveal().reveal('.sponsors__image', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500});
