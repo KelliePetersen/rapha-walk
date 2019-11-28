8286ffa2e6e0195868d794d70248af961006ca0b
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-11-28 15:19:44 -0500

    add final build for main page

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -11,8 +11,8 @@ ScrollReveal().reveal('.hero', {delay: 100, duration: 1500});
 ScrollReveal().reveal('.hero__content-box', {delay: 1500, duration: 2000});
 ScrollReveal().reveal('.hero__logo', {delay: 2500, duration: 1000});
 ScrollReveal().reveal('.scroll-reveal--up', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }});
-ScrollReveal().reveal('.participate__list-item', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500, origin: 'right'});
-ScrollReveal().reveal('.sponsors__image', {delay: 500, duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500});
+ScrollReveal().reveal('.participate__list-item', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 300, origin: 'right'});
+ScrollReveal().reveal('.sponsors__image', {delay: 300, duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 300});
 
 const heroImageInstance = basicScroll.create({
   elem: document.querySelector('.hero__image'),
