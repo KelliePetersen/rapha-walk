73d300c806c9d0d71cc4d90276ec583f858f276a
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-11-28 13:41:41 -0500

    add basicScroll parallax effect to hero image

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,6 +1,7 @@
 import WidescreenMenu from './modules/WidescreenMenu';
 import BackToTop from './modules/BackToTop';
 import MobileMenu from './modules/MobileMenu';
+import * as basicScroll from 'basicscroll';
 
 const widescreenMenu = new WidescreenMenu();
 const backToTop = new BackToTop();
@@ -11,4 +12,22 @@ ScrollReveal().reveal('.hero__content-box', {delay: 1500, duration: 2000});
 ScrollReveal().reveal('.hero__logo', {delay: 2500, duration: 1000});
 ScrollReveal().reveal('.scroll-reveal--up', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }});
 ScrollReveal().reveal('.participate__list-item', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500, origin: 'right'});
-ScrollReveal().reveal('.sponsors__image', {duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500});
+ScrollReveal().reveal('.sponsors__image', {delay: 500, duration: 1000, distance: "50px", viewOffset: { bottom: 200 }, interval: 500});
+
+const instance = basicScroll.create({
+  elem: document.querySelector('.hero__image'),
+  from: 'middle-middle',
+  to: 'bottom-top',
+  props: {
+    '--translateY': {
+      from: 0,
+      to: `${10 * 30}px`
+    },
+    '--scale': {
+      from: 1,
+      to: 1.25
+    }
+  }
+});
+
+instance.start();
